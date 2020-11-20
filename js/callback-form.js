26b5bc39841a5e4a0e3e104d0ec6a57400aaa5f0
Antony <32669563+Tohashow@users.noreply.github.com>
2020-11-20 17:50:38 +0200

    All project fiels

---
+++ js/callback-form.js
@@ -0,0 +1,72 @@
+const callbackForm = document.querySelector('.callback-form-container');
+const RequestReceivedModal = document.querySelector('#request-received');
+
+const userName = document.querySelector('#callback-form-input-name');
+const userEmail = document.querySelector('#callback-form-input-email');
+const userPhone = document.querySelector('#callback-form-input-phone');
+
+userPhone.addEventListener('click', function() {
+    if (!userPhone.value.trim()) {
+        userPhone.value = '+380';
+    }
+});
+
+userPhone.addEventListener('blur', function() {
+    if (userPhone.value === '+380') {
+        userPhone.value = '';
+    }
+});
+
+
+
+callbackForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    let hasError = false;
+
+   if (!userName.value.trim()) {
+       userName.classList.add('collback-form-input-error');
+        hasError = true;
+   } else {
+        userName.classList.remove('collback-form-input-error');
+   }
+
+   if (!userEmail.value.trim() || !isEmailValid(userEmail.value)) {
+        userEmail.classList.add('collback-form-input-error');
+        hasError = true;
+    } else {
+        userEmail.classList.remove('collback-form-input-error');
+       }
+
+
+    if (!userPhone.value.trim() || !isPhoneValid(userPhone.value)) {
+        userPhone.classList.add('collback-form-input-error');
+        hasError = true;
+    } else {
+        userPhone.classList.remove('collback-form-input-error');
+       }      
+
+    if (hasError) {
+        return;
+    }
+
+    userName.value = '';
+    userEmail.value = '';
+    userPhone.value = '';
+
+    RequestReceivedModal.classList.add('modal-active');
+
+    setTimeout(function() {
+        RequestReceivedModal.classList.remove('modal-active');
+    }, 2000);
+});
+
+function isPhoneValid(phone = '') {
+    const regexp = /(\+38)?\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4})/;
+
+    return phone.match(regexp);
+}
+
+function isEmailValid(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email.toLowerCase());
+}
